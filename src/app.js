d14f4148eede2528456cf374f4a0c464ba0277ca
nazar2012 <133564811+nazar2012@users.noreply.github.com>
2025-10-30 12:11:01 +0200

    all

--- src/app.js
+++ src/app.js
@@ -1,6 +1,30 @@
-import { add, multiply } from "./script.js";
+import { add, subtract, multiply, divide } from "./script.js";
 
-const sum = add(5, 3);
-const product = multiply(4, 2);
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
+const select = document.getElementById("operation");
+const result = document.getElementById("result");
+const btn = document.getElementById("calcBtn");
 
-document.getElementById("result").textContent = `Сума: ${sum}, Добуток: ${product}`;
+btn.addEventListener("click", () => {
+    const a = Number(num1.value);
+    const b = Number(num2.value);
+    let res;
+
+    switch (select.value) {
+        case "add":
+            res = add(a, b);
+            break;
+        case "subtract":
+            res = subtract(a, b);
+            break;
+        case "multiply":
+            res = multiply(a, b);
+            break;
+        case "divide":
+            res = divide(a, b);
+            break;
+    }
+
+    result.textContent = `Результат: ${res}`;
+});
--- src/script.js
+++ src/script.js
@@ -1,7 +1,16 @@
 export function add(a, b) {
-  return a + b;
+    return a + b;
+}
+
+export function subtract(a, b) {
+    return a - b;
 }
 
 export function multiply(a, b) {
-  return a * b;
+    return a * b;
+}
+
+export function divide(a, b) {
+    if (b === 0) return "Ділення на нуль!";
+    return a / b;
 }
